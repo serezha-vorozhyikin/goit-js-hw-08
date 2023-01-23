e7b3b5dd477a933a053f34db7373900dee0670e4
serezha-vorozhyikin <serezhavorozhyikin96@gmail.com>
2023-01-23 23:02:50 +0200

    hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  const itemMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+  return itemMarkup;
+}
+
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
